d8767abf99b27edf98353e7a1c7e72d20c2cdd6b
Alex Stout <alexstout009@hotmail.com>
2019-04-13 14:14:16 +0100

    implemented try catch and requestParser

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const { send, error } = require('./util/response');
 const pathFinder = require('./pathFinder');
+const requestParser = require('./util/requestParser');
 
 exports.handler = async (event, context, callback) => {
-  send(callback, await pathFinder(event.to, event.from));
+  try {
+    const { stationTo, stationFrom } = requestParser(event);
+    send(callback, await pathFinder(stationTo, stationFrom));
+  } catch (err) {
+    console.log(err);
+    error(callback, err);
+  }
 };
